65ef6ecaf633baab78a53fa3e06a3b1482ed64f5
Charbel Kafui EKLU <charbeleklu@yahoo.fr>
2022-02-28 02:39:24 +0000

    Identity_User
    Gestion des utilisateurs

---
+++ NetAtlas/NetAtlas/Areas/Identity/Data/NetAtlasUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace NetAtlas.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the NetAtlasUser class
+public class NetAtlasUser : IdentityUser
+{
+}
+
